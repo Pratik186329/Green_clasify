e4df4a447a5a453434755c0568cf15e50a5c82df
Pratik186329 <pratikpawar20011@gmail.com>
2026-02-16 15:05:53 +0530

    Delete Greenclasify---vegetable-prediction-main directory

--- Greenclasify---vegetable-prediction-main/Project code/model.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn as nn
-from torchvision import models
-
-def get_model(num_classes):
-    model = models.mobilenet_v2(pretrained=True)
-    model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
-    return model
